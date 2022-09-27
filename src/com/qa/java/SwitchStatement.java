2e941932cd19838f6377c56d037ded51cfc539c0
DavidWhitfield <davidmwhitfield1992@gmail.com>
2022-09-27 15:05:46 +0100

    switchstatements

---
+++ src/com/qa/java/SwitchStatement.java
@@ -0,0 +1,21 @@
+package com.qa.java;
+
+public class SwitchStatement {
+
+	public static void main(String[] args) {
+	int day = 10;
+	switch (day) {
+	case 1:System.out.println("day 1 schedule");break;
+	case 2:System.out.println("day 2 schedule");break;
+	case 3:System.out.println("day 3 schedule");break;
+	case 4:System.out.println("day 4 schedule");break;
+	case 5:System.out.println("day 5 schedule");break;
+	case 6:System.out.println("day 6 schedule");break;
+	case 7:System.out.println("day 7 schedule");break;
+	default:System.out.println("no schedule found");
+	
+	}
+
+	}
+
+}
