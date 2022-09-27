b3362b766907946ae4b97dbcc5c76c4666740c31
DavidWhitfield <davidmwhitfield1992@gmail.com>
2022-09-27 11:07:45 +0100

    java-fundamental-exercises

---
+++ src/com/qa/data/Types.java
@@ -0,0 +1,21 @@
+package com.qa.data;
+
+class Types {
+
+	public static void main(String[] args) {
+		byte age;
+		age = 25;
+		short icecreams;
+		icecreams = 5200;
+		long allDataUsage;
+		allDataUsage = 526584231L;
+		boolean isDataUsageExceed = allDataUsage > 50000000;
+		int mobiles;
+		mobiles = 67;
+		char mobileColour;
+		mobileColour = "Y";
+		
+
+	}
+
+}
---
+++ src/com/qa/java/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.qa.java;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+	System.out.println("Hi, Welcome to Java programming!");	// TODO Auto-generated method stub
+	System.out.println("java is fun");
+	System.out.println("is it easy");
+	}
+
+}
