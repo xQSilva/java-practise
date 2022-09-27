f9ea2e8de3e53be7a23ca4837b66b6195443bcb6
DavidWhitfield <davidmwhitfield1992@gmail.com>
2022-09-27 15:17:27 +0100

    switchstatements

--- src/com/qa/java/SwitchStatement.java
+++ src/com/qa/java/SwitchStatement.java
@@ -3,19 +3,44 @@ package com.qa.java;
 public class SwitchStatement {
 
 	public static void main(String[] args) {
-	int day = 10;
-	switch (day) {
-	case 1:System.out.println("day 1 schedule");break;
-	case 2:System.out.println("day 2 schedule");break;
-	case 3:System.out.println("day 3 schedule");break;
-	case 4:System.out.println("day 4 schedule");break;
-	case 5:System.out.println("day 5 schedule");break;
-	case 6:System.out.println("day 6 schedule");break;
-	case 7:System.out.println("day 7 schedule");break;
-	default:System.out.println("no schedule found");
-	
-	}
+		int day = 10;
+		switch (day) {
+		case 1:
+			System.out.println("day 1 schedule");
+			break;
+		case 2:
+			System.out.println("day 2 schedule");
+			break;
+		case 3:
+			System.out.println("day 3 schedule");
+			break;
+		case 4:
+			System.out.println("day 4 schedule");
+			break;
+		case 5:
+			System.out.println("day 5 schedule");
+			break;
+		case 6:
+			System.out.println("day 6 schedule");
+			break;
+		case 7:
+			System.out.println("day 7 schedule");
+			break;
+		default:
+			System.out.println("no schedule found");
 
+		}
+		int game = 4;
+		switch (game){
+		case 1:System.out.println("game 1 selected");break;
+		case 2:System.out.println("game 2 selected");break;
+		case 3:System.out.println("game 3 selected");break;
+		case 4:System.out.println("game 4 selected");break;
+		case 5:System.out.println("game 5 selected");break;
+		default:System.out.println("no game selected");
+		
+		}
+		
 	}
 
 }
